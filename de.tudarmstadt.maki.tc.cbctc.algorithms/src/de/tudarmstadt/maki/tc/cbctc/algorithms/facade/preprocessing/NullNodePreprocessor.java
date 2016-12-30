05b2de95af36933e9509cdba59d325af8987d8a7
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2016-12-30 14:26:43 +0100

    Add null implementation of node preprocessor

---
+++ de.tudarmstadt.maki.tc.cbctc.algorithms/src/de/tudarmstadt/maki/tc/cbctc/algorithms/facade/preprocessing/NullNodePreprocessor.java
@@ -0,0 +1,26 @@
+package de.tudarmstadt.maki.tc.cbctc.algorithms.facade.preprocessing;
+
+import de.tudarmstadt.maki.tc.cbctc.algorithms.AbstractTopologyControlAlgorithm;
+import de.tudarmstadt.maki.tc.cbctc.model.Node;
+
+/**
+ * Null implementation of {@link INodePreprocessor}
+ * 
+ * @author Roland Kluge - Initial implementation
+ */
+public class NullNodePreprocessor implements INodePreprocessor
+{
+
+   @Override
+   public void preprocess(Node node)
+   {
+      // nop
+   }
+
+   @Override
+   public void setAlgorithm(AbstractTopologyControlAlgorithm algorithm)
+   {
+      // nop
+   }
+
+}
