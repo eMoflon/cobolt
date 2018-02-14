859d293f9aa4bca1234a10023408558458694ef5
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-02-14 17:11:11 +0100

    Technical progress on experimenting with Henshin CPA

--- org.cobolt.tccpa/src/org/cobolt/tccpa/TopologyControlCriticalPairAnalysisMain.java
+++ org.cobolt.tccpa/src/org/cobolt/tccpa/TopologyControlCriticalPairAnalysisMain.java
@@ -9,7 +9,23 @@
  */
 package org.cobolt.tccpa;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
+import org.eclipse.emf.henshin.cpa.CPAOptions;
+import org.eclipse.emf.henshin.cpa.CPAUtility;
+import org.eclipse.emf.henshin.cpa.CpaByAGG;
+import org.eclipse.emf.henshin.cpa.ICriticalPairAnalysis;
+import org.eclipse.emf.henshin.cpa.UnsupportedRuleException;
+import org.eclipse.emf.henshin.cpa.persist.CriticalPairNode;
+import org.eclipse.emf.henshin.cpa.result.CPAResult;
 import org.eclipse.emf.henshin.interpreter.EGraph;
 import org.eclipse.emf.henshin.interpreter.Engine;
 import org.eclipse.emf.henshin.interpreter.UnitApplication;
@@ -17,6 +33,8 @@ import org.eclipse.emf.henshin.interpreter.impl.EGraphImpl;
 import org.eclipse.emf.henshin.interpreter.impl.EngineImpl;
 import org.eclipse.emf.henshin.interpreter.impl.UnitApplicationImpl;
 import org.eclipse.emf.henshin.model.Module;
+import org.eclipse.emf.henshin.model.Rule;
+import org.eclipse.emf.henshin.model.resource.HenshinResourceFactory;
 import org.eclipse.emf.henshin.model.resource.HenshinResourceSet;
 
 public class TopologyControlCriticalPairAnalysisMain
@@ -76,12 +94,45 @@ public class TopologyControlCriticalPairAnalysisMain
             throw new RuntimeException("");
          }
       }
-
       if (saveResult)
       {
          resourceSet.saveEObject(topology, "topology-output.xmi");
          System.out.println("Saved result file.");
       }
+
+      // Test CPA
+
+      try
+      {
+         Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("henshin", new HenshinResourceFactory());
+         Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("ecore", new EcoreResourceFactoryImpl());
+         Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("henshinCp", new HenshinResourceFactory());
+         final File resultDir = new File("output");
+         resultDir.mkdirs();
+         final String resultsPath = resultDir.getAbsolutePath();
+
+         List<Rule> rules = module.getUnits().stream().filter(unit -> unit instanceof Rule).map(unit -> (Rule) unit).collect(Collectors.toList());
+         ICriticalPairAnalysis cpa = new CpaByAGG();
+         final CPAOptions options = new CPAOptions();
+         options.setComplete(true);
+         options.setIgnore(true);
+         options.setReduceSameRuleAndSameMatch(true);
+         options.persist(resultsPath + "/.cpa.options");
+         cpa.init(rules, options);
+         CPAResult dependencies = cpa.runDependencyAnalysis();
+         CPAResult conflicts = cpa.runConflictAnalysis();
+         CPAResult jointCpaResult = new CPAResult();
+         dependencies.getCriticalPairs().forEach(pair -> jointCpaResult.addResult(pair));
+         conflicts.getCriticalPairs().forEach(pair -> jointCpaResult.addResult(pair));
+
+         HashMap<String, Set<CriticalPairNode>> persistedResults = CPAUtility.persistCpaResult(jointCpaResult, resultsPath);
+         System.out.println("Saved CPA results.");
+
+      } catch (UnsupportedRuleException e)
+      {
+         e.printStackTrace();
+      }
+
    }
 
    public static void main(String[] args)
