2137657ab70dd0fa2a9531ac9cc01f22452bfc48
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-02-22 10:46:14 +0100

    Finally got the unit tests to run!

--- org.cobolt.tccpa/src/org/cobolt/tccpa/TopologyControlCriticalPairAnalysisMain.java
+++ org.cobolt.tccpa/src/org/cobolt/tccpa/TopologyControlCriticalPairAnalysisMain.java
@@ -13,7 +13,6 @@ import java.io.File;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
 import org.eclipse.emf.henshin.cpa.CPAOptions;
@@ -22,12 +21,6 @@ import org.eclipse.emf.henshin.cpa.CpaByAGG;
 import org.eclipse.emf.henshin.cpa.ICriticalPairAnalysis;
 import org.eclipse.emf.henshin.cpa.UnsupportedRuleException;
 import org.eclipse.emf.henshin.cpa.result.CPAResult;
-import org.eclipse.emf.henshin.interpreter.EGraph;
-import org.eclipse.emf.henshin.interpreter.Engine;
-import org.eclipse.emf.henshin.interpreter.UnitApplication;
-import org.eclipse.emf.henshin.interpreter.impl.EGraphImpl;
-import org.eclipse.emf.henshin.interpreter.impl.EngineImpl;
-import org.eclipse.emf.henshin.interpreter.impl.UnitApplicationImpl;
 import org.eclipse.emf.henshin.model.Module;
 import org.eclipse.emf.henshin.model.Rule;
 import org.eclipse.emf.henshin.model.resource.HenshinResourceFactory;
@@ -54,49 +47,6 @@ public class TopologyControlCriticalPairAnalysisMain
 
       // Load the module:
       Module module = resourceSet.getModule("tccpa.henshin", false);
-
-      // Load the example model into an EGraph:
-      EGraph graph = new EGraphImpl(resourceSet.getResource("topology-input.xmi"));
-      System.out.println(graph);
-      final EObject topology = graph.getRoots().get(0);
-      topology.eContents().forEach(o -> {
-         System.out.println(o);
-      });
-
-      // Create an engine and a rule application:
-      Engine engine = new EngineImpl();
-      {
-         UnitApplication removeLink = new UnitApplicationImpl(engine);
-         removeLink.setEGraph(graph);
-         removeLink.setUnit(module.getUnit("removeLink"));
-         removeLink.setParameterValue("linkId", "e12");
-         removeLink.setParameterValue("topology", topology);
-         if (!removeLink.execute(null))
-         {
-            throw new RuntimeException("");
-         }
-      }
-      {
-         UnitApplication addLink = new UnitApplicationImpl(engine);
-         addLink.setEGraph(graph);
-         addLink.setUnit(module.getUnit("addLink"));
-         addLink.setParameterValue("srcId", "n2");
-         addLink.setParameterValue("trgId", "n1");
-         addLink.setParameterValue("linkId", "e21");
-         addLink.setParameterValue("weight", 3.0);
-         addLink.setParameterValue("topology", topology);
-         if (!addLink.execute(null))
-         {
-            throw new RuntimeException("");
-         }
-      }
-      if (saveResult)
-      {
-         resourceSet.saveEObject(topology, "topology-output.xmi");
-         System.out.println("Saved result file.");
-      }
-
-      // Test CPA
 
       try
       {
@@ -135,5 +85,4 @@ public class TopologyControlCriticalPairAnalysisMain
    {
       run(PATH, true); // we assume the working directory is the root of the examples plug-in
    }
-
 }
