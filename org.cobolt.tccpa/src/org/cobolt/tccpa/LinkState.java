86e1f3da83bd79200f9ad28134d32fecfe8b1c1c
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-02-24 22:32:53 +0100

    Add missing file

---
+++ org.cobolt.tccpa/src/org/cobolt/tccpa/LinkState.java
@@ -0,0 +1,17 @@
+package org.cobolt.tccpa;
+
+import tccpa.Link;
+
+/**
+ * Reflects the possible values of {@link Link#getState()}
+ */
+public interface LinkState
+{
+
+   int ACTIVE = 1;
+
+   int UNMARKED = 0;
+
+   int INACTIVE = -1;
+
+}
