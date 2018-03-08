0190a45d32671e5a23ca4c62cb9cb28b91155afa
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-03-08 12:07:25 +0100

    Fix in CPA persistence

--- org.cobolt.tccpa/src/org/cobolt/tccpa/TopologyControlCriticalPairAnalysisMain.java
+++ org.cobolt.tccpa/src/org/cobolt/tccpa/TopologyControlCriticalPairAnalysisMain.java
@@ -13,7 +13,7 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import static java.util.stream.Collectors.toList;
+
 import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -22,167 +22,154 @@ import org.eclipse.emf.henshin.cpa.CPAOptions;
 import org.eclipse.emf.henshin.cpa.CPAUtility;
 import org.eclipse.emf.henshin.cpa.CpaByAGG;
 import org.eclipse.emf.henshin.cpa.ICriticalPairAnalysis;
-import org.eclipse.emf.henshin.cpa.UnsupportedRuleException;
 import org.eclipse.emf.henshin.cpa.result.CPAResult;
 import org.eclipse.emf.henshin.model.Module;
 import org.eclipse.emf.henshin.model.Rule;
 import org.eclipse.emf.henshin.model.resource.HenshinResourceFactory;
 import org.eclipse.emf.henshin.model.resource.HenshinResourceSet;
 
-public class TopologyControlCriticalPairAnalysisMain
-{
-   private static final Logger logger = Logger.getLogger(TopologyControlCriticalPairAnalysisMain.class);
+public class TopologyControlCriticalPairAnalysisMain {
+	private static final Logger logger = Logger.getLogger(TopologyControlCriticalPairAnalysisMain.class);
 
-   /**
-    * Project-relative path to the folder containing models and metamodels
-    */
-   private static final String PATH = "src/org/cobolt/tccpa";
+	/**
+	 * Project-relative path to the folder containing models and metamodels
+	 */
+	private static final String PATH = "src/org/cobolt/tccpa";
 
-   /**
-    * Runs some tests with the specified rules
-    * @param path
-    * @param saveResult
-    */
-   public static void run(String path, boolean saveResult)
-   {
-      final long startTimeMillis = System.currentTimeMillis();
-      // Create a resource set with a base directory:
-      HenshinResourceSet resourceSet = new HenshinResourceSet(path);
+	/**
+	 * Runs some tests with the specified rules
+	 * 
+	 * @param path
+	 * @param saveResult
+	 */
+	public static void run(String path, boolean saveResult) {
+		// Create a resource set with a base directory:
+		HenshinResourceSet resourceSet = new HenshinResourceSet(path);
 
-      // Load the module:
-      Module module = resourceSet.getModule("tccpa.henshin", false);
+		// Load the module:
+		Module module = resourceSet.getModule("tccpa.henshin", false);
 
-      try
-      {
-         Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("henshin", new HenshinResourceFactory());
-         Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("ecore", new EcoreResourceFactoryImpl());
-         Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("henshinCp", new HenshinResourceFactory());
-         final File resultDir = new File("output");
-         resultDir.mkdirs();
-         final String resultsPath = resultDir.getAbsolutePath();
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("henshin", new HenshinResourceFactory());
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("ecore", new EcoreResourceFactoryImpl());
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("henshinCp", new HenshinResourceFactory());
+		final File resultDir = new File("output");
+		resultDir.mkdirs();
+		final String resultsPath = resultDir.getAbsolutePath();
 
-         final List<Rule> rules = module.getUnits().stream().filter(unit -> unit instanceof Rule).map(unit -> (Rule) unit).collect(Collectors.toList());
-         final CPAResult jointCpaResult = new CPAResult();
-         for (final String analysisGoal : Arrays.asList("C"))
-         {
-            final ICriticalPairAnalysis cpa = new CpaByAGG();
+		final List<Rule> rules = module.getUnits().stream().filter(unit -> unit instanceof Rule)
+				.map(unit -> (Rule) unit).collect(Collectors.toList());
+		final CPAResult jointCpaResult = new CPAResult();
+		for (final String analysisGoal : Arrays.asList("C", "D")) {
+			final List<Rule> rulesLeft = rules;
+			final List<Rule> rulesRight = rules;
+			for (final Rule ruleLeft : rulesLeft) {
+				for (final Rule ruleRight : rulesRight) {
+					try {
+						final long startTimeMillis = System.currentTimeMillis();
+						final ICriticalPairAnalysis cpa = new CpaByAGG();
 
-            final CPAOptions options = new CPAOptions();
-            options.setComplete(true);
-            options.setIgnore(false);
-            options.setReduceSameRuleAndSameMatch(false);
-            options.persist(resultsPath + "/.cpa.options");
+						final CPAOptions options = new CPAOptions();
+						options.setComplete(true);
+						options.setIgnore(false);
+						options.setReduceSameRuleAndSameMatch(true);
+						options.persist(resultsPath + "/.cpa.options");
 
-            final List<Rule> rulesLeft = rules;
-            final List<Rule> rulesRight = rules;
-            cpa.init(rulesLeft, rulesRight, options);
+						cpa.init(Arrays.asList(ruleLeft), Arrays.asList(ruleRight), options);
 
-            switch(analysisGoal)
-            {
-            case "D":
-               final CPAResult dependencies = cpa.runDependencyAnalysis(new CPAProgressMonitor(analysisGoal));
-               dependencies.getCriticalPairs().forEach(pair -> jointCpaResult.addResult(pair));
-               break;
-            case "C":
-               final CPAResult conflicts = cpa.runConflictAnalysis(new CPAProgressMonitor(analysisGoal));
-               conflicts.getCriticalPairs().forEach(pair -> jointCpaResult.addResult(pair));
-               break;
-            }
-         }
+						switch (analysisGoal) {
+						case "D":
+							final CPAResult dependencies = cpa
+									.runDependencyAnalysis(new CPAProgressMonitor(analysisGoal));
+							dependencies.getCriticalPairs().forEach(pair -> jointCpaResult.addResult(pair));
+							break;
+						case "C":
+							final CPAResult conflicts = cpa.runConflictAnalysis(new CPAProgressMonitor(analysisGoal));
+							conflicts.getCriticalPairs().forEach(pair -> jointCpaResult.addResult(pair));
+							break;
+						}
+						CPAUtility.persistCpaResult(jointCpaResult, resultsPath);
+						final long endTimeMillis = System.currentTimeMillis();
+						System.out.printf("Saved %d critical pairs after %dms\n",
+								jointCpaResult.getCriticalPairs().size(), (endTimeMillis - startTimeMillis));
+					} catch (final Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
 
-         CPAUtility.persistCpaResult(jointCpaResult, resultsPath);
-         final long endTimeMillis = System.currentTimeMillis();
-         System.out.printf("Saved %d critical pairs after %dms\n", jointCpaResult.getCriticalPairs().size(), (endTimeMillis - startTimeMillis));
+	}
 
-      } catch (final UnsupportedRuleException e)
-      {
-         e.printStackTrace();
-      }
+	public static void main(String[] args) {
+		run(PATH, true); // we assume the working directory is the root of the examples plug-in
+	}
 
-   }
+	private static final class CPAProgressMonitor implements IProgressMonitor {
+		private final String processName;
 
-   public static void main(String[] args)
-   {
-      run(PATH, true); // we assume the working directory is the root of the examples plug-in
-   }
+		private String taskName = "";
 
-   private static final class CPAProgressMonitor implements IProgressMonitor
-   {
-      private final String processName;
+		private String subtaskName = "";
 
-      private String taskName = "";
+		private int workDone;
 
-      private String subtaskName = "";
+		private boolean canceled;
 
-      private int workDone;
+		public CPAProgressMonitor(final String taskName) {
+			this.processName = taskName;
+		}
 
-      private boolean canceled;
+		@Override
+		public void worked(int work) {
+			this.workDone += work;
+			logger.info(String.format("%s Progress: %d work units done", this.formatTask(), this.workDone));
+		}
 
-      public CPAProgressMonitor(final String taskName)
-      {
-         this.processName = taskName;
-      }
+		private String formatTask() {
+			final StringBuilder sb = new StringBuilder();
+			sb.append(processName);
+			if (!this.taskName.isEmpty())
+				sb.append("::").append(this.taskName);
+			if (!this.subtaskName.isEmpty())
+				sb.append("::").append(this.subtaskName);
+			return sb.toString();
+		}
 
-      @Override
-      public void worked(int work)
-      {
-         this.workDone += work;
-         logger.info(String.format("%s Progress: %d work units done", this.formatTask(), this.workDone));
-      }
+		@Override
+		public void subTask(String name) {
+			this.subtaskName = name;
+		}
 
-      private String formatTask()
-      {
-         final StringBuilder sb = new StringBuilder();
-         sb.append(processName);
-         if (!this.taskName.isEmpty())
-            sb.append("::").append(this.taskName);
-         if (!this.subtaskName.isEmpty())
-            sb.append("::").append(this.subtaskName);
-         return sb.toString();
-      }
+		@Override
+		public void setTaskName(String name) {
+			this.taskName = name;
+			this.subtaskName = "";
+		}
 
-      @Override
-      public void subTask(String name)
-      {
-         this.subtaskName = name;
-      }
+		@Override
+		public void setCanceled(boolean value) {
+			this.canceled = value;
+		}
 
-      @Override
-      public void setTaskName(String name)
-      {
-         this.taskName = name;
-         this.subtaskName = "";
-      }
+		@Override
+		public boolean isCanceled() {
+			return this.canceled;
+		}
 
-      @Override
-      public void setCanceled(boolean value)
-      {
-         this.canceled = value;
-      }
+		@Override
+		public void internalWorked(double work) {
+			// Nop
+		}
 
-      @Override
-      public boolean isCanceled()
-      {
-         return this.canceled;
-      }
+		@Override
+		public void done() {
+			System.out.println(this.formatTask() + " completed!");
+		}
 
-      @Override
-      public void internalWorked(double work)
-      {
-         // Nop
-      }
-
-      @Override
-      public void done()
-      {
-         System.out.println(this.formatTask() + " completed!");
-      }
-
-      @Override
-      public void beginTask(String name, int totalWork)
-      {
-         this.setTaskName(name);
-         this.workDone = 0;
-      }
-   }
+		@Override
+		public void beginTask(String name, int totalWork) {
+			this.setTaskName(name);
+			this.workDone = 0;
+		}
+	}
 }
